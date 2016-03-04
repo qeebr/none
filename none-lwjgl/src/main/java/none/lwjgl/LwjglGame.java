c7fa030520e9e44b1d690a524aa0c992e6ffc2c2
qeebr <weber.christian.87@googlemail.com>
2016-03-04 08:16:02 +0100

    Added mechanism to set a window-title

--- none-lwjgl/src/main/java/none/lwjgl/LwjglGame.java
+++ none-lwjgl/src/main/java/none/lwjgl/LwjglGame.java
@@ -26,6 +26,8 @@ public class LwjglGame extends Game {
     public static final int OPENGL_MAJOR_NUMBER = 3;
     public static final int OPENGL_MINOR_NUMBER = 2;
 
+    public static final String STANDARD_TITLE = "Game";
+
     private long lastAction;
     private GameLoop loopImpl;
 
@@ -36,6 +38,8 @@ public class LwjglGame extends Game {
     private Keyboard keyboard;
     private Mouse mouse;
 
+    private String title;
+
     /**
      * CTor for LwjglGame.
      *
@@ -43,8 +47,20 @@ public class LwjglGame extends Game {
      * @param gameLoop The to be executed GameLoop.
      */
     public LwjglGame(GameOptions options, GameLoop gameLoop) {
+        this(STANDARD_TITLE, options, gameLoop);
+    }
+
+    /**
+     * CTor for LwjglGame.
+     *
+     * @param title    The window title.
+     * @param options  Options for this Game.
+     * @param gameLoop The to be executed GameLoop.
+     */
+    public LwjglGame(String title, GameOptions options, GameLoop gameLoop) {
         super(options);
 
+        this.title = Objects.requireNonNull(title, "title");
         this.loopImpl = Objects.requireNonNull(gameLoop);
         this.loopImpl.setGame(this); //Make sure Loop does know us.
     }
@@ -137,6 +153,8 @@ public class LwjglGame extends Game {
             Display.setDisplayMode(new DisplayMode(getOptions().getDisplayWidth(),
                     getOptions().getDisplayHeight()));
             Display.create(pixelFormat, contextAttribs);
+
+            Display.setTitle(title);
         } catch (LWJGLException e) {
             throw new IllegalStateException(e);
         }
